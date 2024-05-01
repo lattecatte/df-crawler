b482621bb9d8839d41efa5a5b9303cb3655127b1
lattecatte <154484150+lattecatte@users.noreply.github.com>
2024-05-01 12:05:35 +0900

    Get label to work, update data array from within radio button func

--- tkinter-test.py
+++ tkinter-test.py
@@ -2,7 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 import sqlite3
 
-def column_sorting(column_name):
+def rb_sort(column_name):
+    global data
     conn = sqlite3.connect("./data/weapons.db")
     c = conn.cursor()
     c.execute(f"SELECT * FROM weapons ORDER BY {column_name} ASC")
@@ -15,45 +16,50 @@ def column_sorting(column_name):
     c.close()
     conn.close()
 
-def on_item_select(event):
-    # Get the selected item
-    selected_index = lb.curselection()
-    print(selected_index)
-    print(selected_index[0])
+# def set_label(var):
+#     var_str = 
+#     var.config(text=f"Name: {item_row[column_dict['name']]}")
+
+def lb_item_select(event):
+    # get index from listbox item select
+    selected_index_tuple = lb.curselection()
+    selected_index = selected_index_tuple[0]
 
     if selected_index:
-        selected_item = lb.get(selected_index[0])
-        print(selected_item)
-        name_label.config(text=f"Name: {selected_item}")
+        # item_name = lb.get(selected_index)
+        item_row = data[selected_index]
+        print(item_row)
+        name_label.config(text=f"Name: {item_row[column_dict['name']]}")
         
 
+# initialize tkinter
 root = tk.Tk()
 root.title = "kekw"
 root.geometry("1000x750")
 
-# connect to db
+# connect to db and fetchall data
 conn = sqlite3.connect("./data/weapons.db")
 c = conn.cursor()
 c.execute("SELECT * FROM weapons ORDER BY name ASC")
+data = c.fetchall()
+c.close()
+conn.close()
 
-# fetch column names
+# get columns
 columns = [i[0] for i in c.description]
+print(columns)
 column_dict = {col: index for index, col in enumerate(columns)}
-
-# tkinter IntVar for tracking selected column
-selected_column = tk.StringVar()
+# tkinter IntVar for tracking radio button selection
+rb_column = tk.StringVar()
 
 # create radio buttons
 for col in columns[0:3]:
-    rb = ttk.Radiobutton(root, text=col, variable=selected_column, value=col, command=lambda c=col: column_sorting(c))
+    rb = ttk.Radiobutton(root, text=col, variable=rb_column, value=col, command=lambda c=col: rb_sort(c))
     rb.pack()
 
 # create listbox
 lb = tk.Listbox(root, width=100, height=30)
 lb.pack()
-
-# fetch data and display in listbox
-data = c.fetchall()
 for row in data:
     lb.insert(tk.END, row[column_dict["name"]])
 
@@ -63,10 +69,7 @@ description_label = tk.Label(root, text="Description:")
 name_label.pack()
 description_label.pack()
 
-# Bind the listbox selection event to the on_item_select function
-lb.bind("<<ListboxSelect>>", on_item_select)
+# update listbox upon item selection
+lb.bind("<<ListboxSelect>>", lb_item_select)
 
-
-c.close()
-conn.close()
 root.mainloop()
