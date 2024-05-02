ec504609c4c64005147ef478c1a5441c33ad6d78
lattecatte <154484150+lattecatte@users.noreply.github.com>
2024-05-03 00:59:32 +0900

    Finish weapon's specials code, still need to work on search button tho

--- tkinter-test.py
+++ tkinter-test.py
@@ -43,28 +43,24 @@ def lb_item_select(event):
         # description
         desc_label = tk.Label(fr, text=f"{item_row[column_dict['description']]}", font=standard12_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
         desc_label.pack(anchor="w", padx=5)
-        # damage string
+        # damage and element
         dmg_str_label = tk.Label(fr, text="Damage:", font=bold12_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
         dmg_str_label.pack(anchor="w", padx=(5,0))
-        # damage and element
         dmg_label = tk.Label(fr, text=f"{item_row[column_dict['damage_min']]}-{item_row[column_dict['damage_max']]}{item_row[column_dict['element']]}", font=standard12_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
         dmg_label.pack(anchor="w", padx=(5,0))
-        # rarity string
+        # rarity
         rrt_str_label = tk.Label(fr, text="Rarity:", font=bold12_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
         rrt_str_label.pack(anchor="w", padx=(5,0))
-        # rarity
         rrt_label = tk.Label(fr, text=f"{item_row[column_dict['rarity']]}", font=standard12_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
         rrt_label.pack(anchor="w", padx=(5,0))
-        # stats string
+        # stats
         stats_str_label = tk.Label(fr, text="Stats:", font=bold12_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
         stats_str_label.pack(anchor="w", padx=(5,0))
-        # stats
         stats_label = tk.Label(fr, text=f"{item_row[column_dict['bonuses']].lstrip()}", font=standard12_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
         stats_label.pack(anchor="w", padx=(5,0))
-        # resists string
+        # resists
         resists_str_label = tk.Label(fr, text="Resists:", font=bold12_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
         resists_str_label.pack(anchor="w", padx=(5,0))
-        # resists
         resists_label = tk.Label(fr, text=f"{item_row[column_dict['resists']].lstrip()}", font=standard12_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
         resists_label.pack(anchor="w", padx=(5,0))
         # dummy label
@@ -72,6 +68,7 @@ def lb_item_select(event):
         dummy_label.pack()
         # special
         if item_row[column_dict['special_name']]:
+            sp_labels = []
             sp_name_str_label = tk.Label(fr, text="Special Name:", font=bold12_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
             sp_name_str_label.pack(anchor="w", padx=(5,0))
             sp_name_label = tk.Label(fr, text=f"{item_row[column_dict['special_name']].lstrip()}", font=standard10_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
@@ -80,28 +77,49 @@ def lb_item_select(event):
             sp_act_str_label.pack(anchor="w", padx=(5,0))
             sp_act_label = tk.Label(fr, text=f"{item_row[column_dict['special_activation']].lstrip()}", font=standard10_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
             sp_act_label.pack(anchor="w", padx=(5,0))
+            sp_eff_str_label = tk.Label(fr, text="Special Effect:", font=bold12_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
+            sp_eff_str_label.pack(anchor="w", padx=(5,0))
+            sp_eff_label = tk.Label(fr, text=f"{item_row[column_dict['special_effect']].lstrip()}", font=standard10_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
+            sp_eff_label.pack(anchor="w", padx=(5,0))
+            sp_dmg_str_label = tk.Label(fr, text="Special Damage:", font=bold12_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
+            sp_dmg_str_label.pack(anchor="w", padx=(5,0))
+            sp_dmg_label = tk.Label(fr, text=f"{item_row[column_dict['special_damage']]}", font=standard10_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
+            sp_dmg_label.pack(anchor="w", padx=(5,0))
+            sp_elem_str_label = tk.Label(fr, text="Special Element:", font=bold12_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
+            sp_elem_str_label.pack(anchor="w", padx=(5,0))
+            sp_elem_label = tk.Label(fr, text=f"{item_row[column_dict['special_element']].lstrip()}", font=standard10_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
+            sp_elem_label.pack(anchor="w", padx=(5,0))
+            sp_dtype_str_label = tk.Label(fr, text="Special Damage Type:", font=bold12_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
+            sp_dtype_str_label.pack(anchor="w", padx=(5,0))
+            sp_dtype_label = tk.Label(fr, text=f"{item_row[column_dict['special_damage_type']].lstrip()}", font=standard10_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
+            sp_dtype_label.pack(anchor="w", padx=(5,0))
+
+            sp_rate_str_label = tk.Label(fr, text="Special Rate:", font=bold12_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
+            sp_rate_str_label.pack(anchor="w", padx=(5,0))
+            sp_rate_label = tk.Label(fr, text=f"{item_row[column_dict['special_rate']].lstrip()}", font=standard10_font, bg="#ebe2c5", justify="left", wraplength=fr_width-4*fr_padx)
+            sp_rate_label.pack(anchor="w", padx=(5,0))
 
 def write_callback(*args):
     keyword = sv.get()
     print(keyword)
 
-    # global data
-    # conn = sqlite3.connect("./data/weapons.db")
-    # c = conn.cursor()
-    # c.execute(f"SELECT * FROM weapons WHERE ? ORDER BY {rb_column} ASC", ())
-    # data = c.fetchall()
+    global data
+    conn = sqlite3.connect("./data/weapons.db")
+    c = conn.cursor()
+    c.execute(f"SELECT * FROM weapons WHERE name = ? ORDER BY {rb_column} ASC", (keyword,))
+    data = c.fetchall()
 
-    # lb.delete(0, tk.END)
-    # for row in data:
-    #     lb.insert(tk.END, row[column_dict["name"]])
+    lb.delete(0, tk.END)
+    for row in data:
+        lb.insert(tk.END, row[column_dict["name"]])
 
-    # c.close()
-    # conn.close()
+    c.close()
+    conn.close()
 
 # initialize tkinter
 root = tk.Tk(className="DFCrawler")
 root.configure(bg="#eacea6")
-root.geometry("800x800")
+root.geometry("800x1200")
 
 # fonts
 standard10_font = font.Font(family="Helvetica", size=10)
@@ -126,7 +144,7 @@ column_dict = {col: index for index, col in enumerate(columns)}
 custom_font = font.Font(family="Helvetica", size=10, weight="bold")
 # custom_style = ttk.Style()
 
-lb = tk.Listbox(root, width=30, height=30, font=custom_font, bg="#ebe2c5", selectbackground="#cbd8fe", selectborderwidth=2, relief=tk.SOLID)
+lb = tk.Listbox(root, width=30, height=40, font=custom_font, bg="#ebe2c5", selectbackground="#cbd8fe", selectborderwidth=2, relief=tk.SOLID)
 lb.grid(column=0, row=0, sticky=tk.NW, padx=5, pady=5)
 for row in data:
     lb.insert(tk.END, row[column_dict["name"]])
